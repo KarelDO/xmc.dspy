a3f5b9dbab4daf04292780b766182b886d83c3cc
Karel D'Oosterlinck <kareldoosterlinck@gmail.com>
2024-01-30 14:15:28 -0800

    fix: made techwolf load future proof

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,1 +1,8 @@
-mkdir -p data/esco && git clone https://github.com/jensjorisdecorte/Skill-Extraction-benchmark.git data/esco
+mkdir -p data/esco 
+cd data/esco
+git clone https://github.com/jensjorisdecorte/Skill-Extraction-benchmark.git
+cd Skill-Extraction-benchmark
+git checkout 157da05a24e6ecfee82e4b5d01cba68a2ed0552f
+mv * ..
+cd ../../..
+rm -rf data/esco/Skill-Extraction-benchmark/
